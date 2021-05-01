94ddbd4e8850246fd4de3f2a0c60139a881d74d1
99nk <99.niharikakumar@gmail.com>
2021-05-01 21:46:18 +0530

    Skills adding option

--- app/src/main/java/com/example/sheaholics_app/RegisterActivity.java
+++ app/src/main/java/com/example/sheaholics_app/RegisterActivity.java
@@ -1,14 +1,139 @@
 package com.example.sheaholics_app;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.annotation.NonNull;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText mName,mEmail,mPassword,mAge;
+    Button mRegister;
+    public String email,password;
+    TextView mLogin;
+    FirebaseAuth mAuth;
+    ProgressBar progressBar;
+    DatabaseReference userRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mName=findViewById(R.id.register_name);
+        mEmail=findViewById(R.id.register_email);
+        mPassword=findViewById(R.id.register_password);
+        mAge=findViewById(R.id.register_age);
+        mRegister=findViewById(R.id.register_button);
+        mLogin=findViewById(R.id.registerToLogin);
+        mAuth=FirebaseAuth.getInstance();
+        progressBar=findViewById(R.id.progressBar);
+        userRef= FirebaseDatabase.getInstance().getReference().child("Users");
+        if(mAuth.getCurrentUser()!=null)
+        {
+            startActivity(new Intent(getApplicationContext(),SplashScreen.class));
+            finish();
+        }
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email=mEmail.getText().toString().trim();
+                password=mPassword.getText().toString().trim();
+                if(TextUtils.isEmpty(email))
+                {
+                    mEmail.setError("Email is req");
+                    return;
+                }
+                if(TextUtils.isEmpty(password))
+                {
+                    mPassword.setError("Password is req");
+                    return;
+                }
+                if(password.length()<6)
+                {
+                    mPassword.setError("Min 6 char password");
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            //FirebaseUser user=mAuth.getCurrentUser();
+                            final String current_user_id=mAuth.getCurrentUser().getUid();
+                            final DatabaseReference UserRef= FirebaseDatabase.getInstance().getReference().child("Users");
+                            UserRef.addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                    if(!dataSnapshot.hasChild(current_user_id))
+                                    {
+                                        String id =mAuth.getCurrentUser().getUid().toString();
+                                        String Mail=email;
+                                        String Phone=mAge.getText().toString().trim();
+                                        String Name=mName.getText().toString().trim();
+                                        HashMap user1=new HashMap();
+                                        user1.put("name",Name);
+                                        user1.put("email",Mail);
+                                        user1.put("id",id);
+                                        user1.put("age",Phone);
+                                        UserRef.child(id).updateChildren(user1).addOnCompleteListener(new OnCompleteListener() {
+                                            @Override
+                                            public void onComplete(@NonNull Task task) {
+                                                if(task.isSuccessful())
+                                                {
+                                                    Toast.makeText(RegisterActivity.this, "Details added", Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                }
+                            });
+                            Toast.makeText(RegisterActivity.this, "User Created", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),SkillsSetup.class));
+                        }
+                        else
+                        {
+                            progressBar.setVisibility(View.GONE);
+                            Toast.makeText(RegisterActivity.this, "Error "+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+            }
+        });
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                finish();
+            }
+        });
     }
 }
+
+
